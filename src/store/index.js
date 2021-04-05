8ad73b0820edffbbee448014a5ee79f587a998f0
James Muriithi <muriithijames556@gmail.com>
2021-04-05 20:16:10 +0300

    added store configs

---
+++ src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from "vuex";
+import recipesModule from "./modules/recipes";
+
+const store = createStore({
+    modules: {
+        recipes: recipesModule
+    }
+});
+
+export default store;
---
+++ src/store/modules/recipes/index.js
@@ -0,0 +1,12 @@
+import actions from "./actions.js";
+import getters from "./getters.js";
+import mutations from "./mutations.js";
+
+export default {
+  state() {
+    return {};
+  },
+  mutations,
+  actions,
+  getters,
+};
