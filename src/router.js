c0daba7bdc42313e5a34954dea7a78e7a3c8f853
James Muriithi <muriithijames556@gmail.com>
2021-04-05 20:41:20 +0300

    added routes

---
+++ src/router.js
@@ -0,0 +1,10 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import RecipeList from './pages/recipes/RecipeList.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [{ path: "/", component: RecipeList }],
+});
+
+export default router;
