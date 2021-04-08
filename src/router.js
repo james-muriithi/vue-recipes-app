62e0e82a49b7994dc5df77cf233221cb0582c643
James Muriithi <muriithijames556@gmail.com>
2021-04-08 12:17:32 +0300

    added 404 page

--- src/router.js
+++ src/router.js
@@ -1,10 +1,14 @@
 import { createRouter, createWebHistory } from "vue-router";
 
 import RecipeList from "./pages/recipes/RecipeList.vue";
+import NotFound from "./pages/NotFound.vue";
 
 const router = createRouter({
   history: createWebHistory(),
-  routes: [{ path: "/", component: RecipeList }]
+  routes: [
+    { path: "/", component: RecipeList },
+    { path: "/:notFound(.*)", component: NotFound }
+  ]
 });
 
 export default router;
